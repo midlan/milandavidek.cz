4d8fde9de6a2681fbb0ef1191a92c52bf4a33aef
Milan Davídek <me@milandavidek.cz>
2021-04-11 02:58:30 +0200

    migrated

---
+++ public/index.php
@@ -0,0 +1,51 @@
+<?php
+
+header('Content-Type: text/html; charset=utf-8');
+
+?><!DOCTYPE html>
+<html lang="cs">
+  <head>
+    <meta name="author" content="Milan Davídek" />
+    <meta name="description" content="" />
+    <title>Milan Davídek - Osobní stránky</title>
+  </head>
+  <body>
+    <h1>Vítejte na mých osobních stránkách</h1>
+    <p>
+      Ahoj, jmenuji se Milan Davídek a tohle jsou mé osobní stránky. Zatím jen provizorní, napsané za 5 minut, ale v budoucnu tu najdete moje projekty, informace o mně atd.
+    </p>
+    <h2>Moje projekty</h2>
+    <p>
+      <i>Seřazené od nejnovějšího po nejstarší.</i>
+    </p>
+    <ul>
+      <li><a href="http://milandavidek.cz/">Osobní stránky, které právě čtete</a></li>
+      <li><a href="http://www.oneksys.cz/">ONEKSYS - Online ekonomický systém</a></li>
+      <li><a href="http://jaknawebovky.php5.cz/javascript-sibenice.php">Hra Šibenice</a></li>
+      <li><a href="http://jaknawebovky.php5.cz/">JakNaWebovky - HTML a CSS manuál</a></li>
+    </ul>
+    <h2>Fakturační údaje</h2>
+    <p>
+        Milan Davídek<br />
+        IČ: 01980432<br />
+        DIČ: CZ9404131077<br />
+        Svojkovice 18<br />
+        338 22 Svojkovice
+    </p>
+    <h2>Affiliate</h2>
+    <p>
+        Pokud jsem vám poradil s výběrem hardware, budu rád, když ho nakoupíte v některém z partnerských obchodů:
+    </p>
+    <ul>
+        <li><a href="https://www.alza.cz/?idp=6325">Alza.cz</a></li>
+    </ul>
+<!--
+Doručovací adresa:
+
+ATELI Development s.r.o.
+Milan Davídek
+Habrmannovo nám. 794/5
+312 00 Plzeň
+-->
+  </body>
+</html>
